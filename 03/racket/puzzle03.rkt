c7e763dc0d73ab659b92169c87794e3d98914c9e
Joseph Ramaswami <jramaswami@gmail.com>
2019-12-03 13:37:05 -0500

    Refactored solution to day 3 in Racket.

--- 03/racket/puzzle03.rkt
+++ 03/racket/puzzle03.rkt
@@ -14,59 +14,64 @@
 (define (get-offset dirn)
   (second (assoc dirn offsets)))
 
-
-
-;; Get next posn based on offset.
-(define (next-posn posn offset)
-  (list (+ (first posn) (first offset))
-        (+ (second posn) (second offset))))
-
-;; Record visit to posn, returning posn.
-(define (visit-posn posn steps visited)
-  (when (not (dict-has-key? visited posn))
-    (dict-set! visited posn steps))
-  posn)
-
-;; Move and record visit to each intermediate posn.
-(define (move cmd posn steps visited)
-  (define dirn (substring cmd 0 1))
-  (define dist (string->number (substring cmd 1)))
-  (define offset (get-offset dirn))
-  (define (move-loop d posn0 steps0)
-    (if (= d 0) (values posn0 steps0)
-        (move-loop (sub1 d)
-                   (visit-posn (next-posn posn0 offset) (add1 steps0) visited)
-                   (add1 steps0))))
-  (move-loop dist posn steps))
-
+;; Solve puzzle.
 (define (solve wires)
-  (define wire0 (first wires))
-  (define wire1 (second wires))
-  (define visited0 (make-hash))
-  (define visited1 (make-hash))
+  ;; Run through the commands for a given wire.
   (define (run-wire wire posn steps visited)
-    (when (not (empty? wire))
-      (let ([cmd (first wire)])
-        (let-values ([(posn0 steps0) (move cmd posn steps visited)])
-          (run-wire (rest wire) posn0 steps0 visited)))))
+    ;; Move and record visit to each intermediate posn.
+    (define (move cmd posn steps visited)
+      ;; Get next posn based on offset.
+      (define (next-posn posn0 offset0)
+        (list (+ (first posn0) (first offset0))
+              (+ (second posn0) (second offset0))))
+      ;; Record visit to posn, returning posn.
+      (define (visit-posn posn0 steps0 visited0)
+        (when (not (dict-has-key? visited0 posn0))
+          (dict-set! visited0 posn0 steps0))
+        posn0)
+      ;; Loop over each posn in loop.
+      (define (move-loop offset d posn0 steps0)
+        (if (= d 0) (values posn0 steps0)
+            (move-loop offset
+                       (sub1 d)
+                       (visit-posn (next-posn posn0 offset) (add1 steps0) visited)
+                       (add1 steps0))))
+      ;; First call to move-loop.
+      (move-loop (get-offset (substring cmd 0 1))
+                 (string->number (substring cmd 1))
+                 posn
+                 steps))
+    ;; Recursively run the commands for the given wire.
+    (if (empty? wire)
+        visited
+        (let-values ([(posn0 steps0) (move (first wire) posn steps visited)])
+          (run-wire (rest wire) posn0 steps0 visited))))
+  ;; Return the manhattan distance to the origin.
+  (define (distance-to-origin posn)
+    (+ (abs (first posn)) (abs (second posn))))
+  ;; Return the total number of steps by wire0 and wire1 to reach the given posn.
+  (define (steps-to-posn posn visited0 visited1)
+    (+ (dict-ref visited0 posn) (dict-ref visited1 posn)))
+  ;; Loop through the visited posns for the two wires to find the minimum distance
+  ;; to the origin and the minimum number of steps to any given position touched by
+  ;; both wires.
   (define (find-solution visited0 visited1)
-    (define (soln-loop ks min-steps min-dist)
-      (if (empty? ks)
+    (define (soln-loop posns min-steps min-dist)
+      (if (empty? posns)
           (values min-dist  min-steps)
-          (let ([k (first ks)])
-            (if (dict-has-key? visited1 k)
-                (let ([steps (+ (dict-ref visited0 k) (dict-ref visited1 k))]
-                      [dist (+ (abs (first k)) (abs (second k)))])
-                  (soln-loop (rest ks) (min min-steps steps) (min min-dist dist)))
-                (soln-loop (rest ks) min-steps min-dist)))))
+          (if (dict-has-key? visited1 (first posns))
+              (soln-loop (rest posns)
+                         (min min-steps (steps-to-posn (first posns) visited0 visited1))
+                         (min min-dist (distance-to-origin (first posns))))
+              (soln-loop (rest posns) min-steps min-dist))))
     (soln-loop (dict-keys visited0) 9999999999 9999999999))
-  (run-wire wire0 (list 0 0) 0 visited0)
-  (run-wire wire1 (list 0 0) 0 visited1)
-  (find-solution visited0 visited1))
+  ;; Find the solution ... returning both the minimum distance and steps.
+  (find-solution (run-wire (first wires) (list 0 0) 0 (make-hash))
+                 (run-wire (second wires) (list 0 0) 0 (make-hash))))
 
 (module+ main
-(require racket/file)
-(define wires (map (λ (x) (string-split (string-trim x) ",")) (file->lines "../input.txt")))
+  (require racket/file)
+  (define wires (map (λ (x) (string-split (string-trim x) ",")) (file->lines "../input.txt")))
   (let-values ([(part1-soln part2-soln) (solve wires)])
     (printf "The solution to part 1 is ~a.~n" part1-soln)
     (printf "The solution to part 2 is ~a.~n" part2-soln)))
