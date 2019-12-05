b6379d2a9afbce210ab6e7a2359e8716074777dc
Joseph Ramaswami <jramaswami@gmail.com>
2019-12-05 08:28:32 -0500

    Solution to day 5 in Tcl.

--- 05/tcl/puzzle05.tcl
+++ 05/tcl/puzzle05.tcl
@@ -5,14 +5,31 @@
 source "../../lib/tcl/intcode.tcl"
 
 if {$::argv0 == [info script]} {
-    puts "\tBooting ....."
+    puts [string repeat "*" 80]
+    puts "For part 1, enter 1 when asked for input."
+    puts "The answer will be the last output."
+    puts "\tBooting intcode computer ....."
     set filename [lindex $::argv 0]
-    puts "\tLoading code ...."
+    puts "\tLoading code from $filename ...."
     set filep [open $filename r]
     set input [string trimright [read $filep]]
     close $filep
     set intcode [split $input ","]
-    puts "\tRunning ....."
+    puts "\tRunning code from $filename ....."
     run_intcode $intcode
     puts "\tDone!"
+    puts [string repeat "*" 80]
+    puts "For part 2, enter 5 when asked for input."
+    puts "The answer will be the last output."
+    puts "\tBooting intcode computer ....."
+    set filename [lindex $::argv 0]
+    puts "\tLoading code from $filename ...."
+    set filep [open $filename r]
+    set input [string trimright [read $filep]]
+    close $filep
+    set intcode [split $input ","]
+    puts "\tRunning code from $filename ....."
+    run_intcode $intcode
+    puts "\tDone!"
+
 }
--- lib/tcl/intcode.tcl
+++ lib/tcl/intcode.tcl
@@ -52,13 +52,82 @@ proc op3 {instruction_pointer_var intcode_var parameter} {
     incr instruction_pointer 2
 }
 
+# Opcode 4 outputs the value of its only parameter. For example, the
+# instruction 4,50 would output the value at address 50.
 proc op4 {instruction_pointer_var intcode_var parameter} {
     upvar $instruction_pointer_var instruction_pointer
     upvar $intcode_var intcode
-    set index [lindex $intcode [expr {$instruction_pointer + 1}]]
-    set value [lindex $intcode $index]
+    # set index [lindex $intcode [expr {$instruction_pointer + 1}]]
+    # set value [lindex $intcode $index]
+    # puts "output $index $value @ $instruction_pointer parameter $parameter"
+    set value [get_value $intcode $instruction_pointer $parameter 1]
+    # puts "output $value @ $instruction_pointer parameter $parameter"
     puts "Output: $value"
     incr instruction_pointer 2
+}
+
+# Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the
+# instruction pointer to the value from the second parameter. Otherwise, it
+# does nothing.
+proc op5  {instruction_pointer_var intcode_var parameter} {
+    upvar $instruction_pointer_var instruction_pointer
+    upvar $intcode_var intcode
+    set lhs [get_value $intcode $instruction_pointer $parameter 1]
+    if {$lhs != 0} {
+        set rhs [get_value $intcode $instruction_pointer $parameter 2]
+        set instruction_pointer $rhs
+    } else {
+        incr instruction_pointer 3
+    }
+}
+
+# Opcode 6 is jump-if-false: if the first parameter is zero, it sets the
+# instruction pointer to the value from the second parameter. Otherwise, it
+# does nothing.
+proc op6 {instruction_pointer_var intcode_var parameter} {
+    upvar $instruction_pointer_var instruction_pointer
+    upvar $intcode_var intcode
+    set lhs [get_value $intcode $instruction_pointer $parameter 1]
+    if {$lhs == 0} {
+        set rhs [get_value $intcode $instruction_pointer $parameter 2]
+        set instruction_pointer $rhs
+    } else {
+        incr instruction_pointer 3
+    }
+}
+
+# Opcode 7 is less than: if the first parameter is less than the second
+# parameter, it stores 1 in the position given by the third parameter.
+# Otherwise, it stores 0.
+proc op7 {instruction_pointer_var intcode_var parameter} {
+    upvar $instruction_pointer_var instruction_pointer
+    upvar $intcode_var intcode
+    set lhs [get_value $intcode $instruction_pointer $parameter 1]
+    set rhs [get_value $intcode $instruction_pointer $parameter 2]
+    set dest_index [lindex $intcode [expr {$instruction_pointer + 3}]]
+    if {$lhs < $rhs} {
+        lset intcode $dest_index 1
+    } else {
+        lset intcode $dest_index 0
+    }
+    incr instruction_pointer 4
+}
+
+# Opcode 8 is equals: if the first parameter is equal to the second parameter,
+# it stores 1 in the position given by the third parameter. Otherwise, it
+# stores 0.
+proc op8 {instruction_pointer_var intcode_var parameter} {
+    upvar $instruction_pointer_var instruction_pointer
+    upvar $intcode_var intcode
+    set lhs [get_value $intcode $instruction_pointer $parameter 1]
+    set rhs [get_value $intcode $instruction_pointer $parameter 2]
+    set dest_index [lindex $intcode [expr {$instruction_pointer + 3}]]
+    if {$lhs == $rhs} {
+        lset intcode $dest_index 1
+    } else {
+        lset intcode $dest_index 0
+    }
+    incr instruction_pointer 4
 }
 
 proc run_intcode {intcode} {
@@ -72,6 +141,7 @@ proc run_intcode {intcode} {
         set token [lindex $intcode $instruction_pointer]
         set opcode [expr {$token % 100}]
         set parameter [format %03d [expr {int($token / 100)}]]
+        # puts "$opcode $parameter @ $instruction_pointer"
     }
     return [lindex $intcode 0]
 }
