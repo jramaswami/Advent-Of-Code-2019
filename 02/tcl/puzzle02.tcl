f54480444aca13bf03b1c82e211c3c02288efa5f
Joseph Ramaswami <jramaswami@gmail.com>
2019-12-12 11:48:24 -0500

    Solution to day 2 in Tcl using OO.

--- 02/tcl/puzzle02.tcl
+++ 02/tcl/puzzle02.tcl
@@ -1,27 +1,71 @@
 # Advent of Code 2019 :: Day 2 :: 1202 Program Alarm
 # https://adventofcode.com/2019/day/2
 
-# Include Intcode Computer
-source "../../lib/tcl/intcode.tcl"
+oo::class create IntcodeComputer {
+    variable intcode instruction_pointer
 
-proc set_verb_and_noun {intcode noun verb} {
-    lset intcode 1 $noun
-    lset intcode 2 $verb
-    return $intcode
+    constructor {intcode0} {
+        my variable intcode
+        set intcode $intcode0
+    }
+
+    method op1 {instruction_pointer_var intcode_var} {
+        my variable instruction_pointer intcode
+
+        set lhs_index [lindex $intcode [expr {$instruction_pointer + 1}]]
+        set rhs_index [lindex $intcode [expr {$instruction_pointer + 2}]]
+        set dest_index [lindex $intcode [expr {$instruction_pointer + 3}]]
+
+        set lhs [lindex $intcode $lhs_index]
+        set rhs [lindex $intcode $rhs_index]
+        set res [expr {$lhs + $rhs}]
+        lset intcode $dest_index $res
+
+        incr instruction_pointer 4
+    }
+
+	method op2 {instruction_pointer_var intcode_var} {
+        my variable instruction_pointer intcode
+
+		set lhs_index [lindex $intcode [expr {$instruction_pointer + 1}]]
+		set rhs_index [lindex $intcode [expr {$instruction_pointer + 2}]]
+		set dest_index [lindex $intcode [expr {$instruction_pointer + 3}]]
+
+		set lhs [lindex $intcode $lhs_index]
+		set rhs [lindex $intcode $rhs_index]
+		set res [expr {$lhs * $rhs}]
+		lset intcode $dest_index $res
+
+		incr instruction_pointer 4
+	}
+
+	method run {noun verb} {
+        my variable instruction_pointer intcode
+        set instruction_pointer 0
+
+		lset intcode 1 $noun
+		lset intcode 2 $verb
+
+        set opcode [lindex $intcode $instruction_pointer]
+        while {$opcode != 99} {
+			my op${opcode} instruction_pointer intcode
+            set opcode [lindex $intcode $instruction_pointer]
+		}
+        return [lindex $intcode 0]
+	}
+
 }
 
 proc solve_part1 {intcode} {
-    Intcode::init [set_verb_and_noun $intcode 12 2] {}
-    lassign [Intcode::run] intcode0 output
-    return [lindex $intcode0 0]
+    set computer [IntcodeComputer new $intcode]
+    return [$computer run 12 2]
 }
 
 proc solve_part2 {intcode} {
     for {set noun 0} {$noun < 100} {incr noun} {
         for {set verb 0} {$verb < 100} {incr verb} {
-            Intcode::init [set_verb_and_noun $intcode $noun $verb] {}
-            lassign [Intcode::run] intcode0 output
-            set result [lindex $intcode0 0]
+            set computer [IntcodeComputer new $intcode]
+            set result [$computer run $noun $verb]
             if {$result == 19690720} {
                 return [expr {100 * $noun + $verb}]
             }
